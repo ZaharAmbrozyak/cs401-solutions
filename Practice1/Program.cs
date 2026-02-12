a93de83a9e04da02a00e0e1ed5ee14437064b61b
Zahar <60888814+ZaharAmbrozyak@users.noreply.github.com>
2026-02-12 18:00:08 +0200

    Start of the practice lesson

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace practice1
+{
+    class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Practice #1");
+        }
+    }
+}
