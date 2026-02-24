c4223b6842dd73b7be90bba520a6e8bd588e5163
Zahar <60888814+ZaharAmbrozyak@users.noreply.github.com>
2026-02-24 08:48:29 +0200

    ending prep, starting lesson 3

--- prep3/Program.cs
+++ prep3/Program.cs
@@ -2,12 +2,29 @@
 
 class Program
 {
+    class BankAccount
+    {
+        private int currentAmount;
+        public BankAccount(int initialAmount = 0)
+        {
+            currentAmount = initialAmount;
+        }
+        public void Deposit(int amount)
+        {
+            if (amount <= 0) throw new Exception("Amount should be positive");
+            currentAmount += amount;
+        }
+        public int GetCurrentAmount() => currentAmount;
+    }
+
+   
     static void Main(string[] args)
     {
-        Rectangle rectangle1 = new Rectangle(1, 2);
-        Rectangle rectangle2 = new Rectangle(3, 4);
         
-        Console.WriteLine();
+        var acc = new BankAccount(100);
+        acc.Deposit(60);
+        acc.Deposit(15);
+        Console.WriteLine(acc.GetCurrentAmount());
 
     }
 }
